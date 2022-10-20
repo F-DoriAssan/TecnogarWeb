2d9ef9447ba72661c9a2e874430c565e68ec977c
Federico Dorigatti Assan <114174311+F-DoriAssan@users.noreply.github.com>
2022-10-20 02:08:30 -0300

    Hago el btn del nav para celular, configuro su nav, realizo js para la apertura y el cierre. Dia muy estresante y cansador! Mañana debo terminar el responsive de celular

--- js/main.js
+++ js/main.js
@@ -5,3 +5,20 @@ function celuNosotros(){
 function navCelu(){
   document.getElementById("idDiv").classList.toggle("navCelu");
 }
+$(document).ready(function(){
+    var $divContenedor = $('#div-contenedor-bloqueo');
+    var $body = $('html,body');
+
+    $('#ventana-emergente').on('click', function() {
+        $divContenedor.fadeIn();
+        $body.addClass('bloqueo-scroll'); // clase de manejo
+    });
+    $('#div-superpone').on('click', function() {
+        $divContenedor.fadeOut();
+        $body.removeClass('bloqueo-scroll'); // clase de manejo
+    });
+});
+function openFunctions() {
+  var div = document.getElementById("functions");
+  div.classList.toggle("block");
+}
